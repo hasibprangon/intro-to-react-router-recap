ca658fa055284cdfe220643df9b84ed8aa7b582e
hasibprangon <hasibulhossainp09@gmail.com>
2024-01-06 15:16:43 +0600

    creating simple router

--- src/main.jsx
+++ src/main.jsx
@@ -2,35 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
-import ErrorPage from './Components/ErrorPage/ErrorPage.jsx'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const router = createBrowserRouter([
   {
-    path:'/',
-    element: <App></App>
-  },
-  {
-    path:'/about',
-    element:<div>this is about page</div>
-  },
-  {
-    path:'/contact',
-    element:<div>01710792327</div>
-  },
-  {
-    path:'/',
-    element:<div>hello</div>,
-    errorElement:<ErrorPage></ErrorPage>
+    path: '/',
+    element:<div>hello</div>
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )

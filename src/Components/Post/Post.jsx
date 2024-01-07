0e47ce130190877213fe37c1fa1077dfbd470f97
hasibprangon <hasibulhossainp09@gmail.com>
2024-01-07 14:49:31 +0600

    using navigate to loading data

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Post.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 const Post = ({post}) => {
     // console.log(post);
     const {id, title} = post;
+    const navigate = useNavigate();
+
+    const handleNevigation = () => {
+            navigate(`/post/${id}`)
+    }
     return (
         <div className='post'>
             <h2>{title}</h2>
             <p> <Link to={`/post/${id}`}>See more</Link> </p>
             <Link to={`/post/${id}`}><button>Show Detail</button></Link>
+            <button onClick={handleNevigation}>With Button Handler</button>
         </div>
     );
 };
--- src/Components/PostDetail/PostDetail.jsx
+++ src/Components/PostDetail/PostDetail.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetail = () => {
     const postD = useLoaderData();
     console.log(postD);
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    };
     return (
         <div>
             <h2>Number of post: {postD.id}</h2>
-           <p>{postD.body}</p>
+            <p>{postD.body}</p>
+            <button onClick={handleGoBack}>Go back</button>
         </div>
     );
 };
